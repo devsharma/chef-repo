31fc440f2653dd4f26288677363ec520f6ab91af
Dev Sharma <devs@one.com>
2014-09-24 08:10:05 +0530

    all the packages installed

---
+++ cookbooks/demo/metadata.rb
@@ -0,0 +1,7 @@
+name             'demo'
+maintainer       'One.com'
+maintainer_email 'devs@one.com'
+license          'All rights reserved'
+description      'Installs/Configures demo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
